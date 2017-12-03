28a10f33dfb76e495ee445c77c1f29ad32d76ea7
sarvesh15 <34206277+sarvesh15@users.noreply.github.com>
2017-12-03 21:29:41 +0900

    Update 2-1-remove-dups.cpp
    Freeing dynamically allocated memory for duplicated nodes

--- chapter-2-Linked-Lists/2-1-remove-dups.cpp
+++ chapter-2-Linked-Lists/2-1-remove-dups.cpp
@@ -1,10 +1,11 @@
+/* Small memory leak  bug fix in the code submitted by @mandliya */
 /*
- * Cracking the coding interview edition 6
- * Problem 2-1 : Remove duplicates from an unsorted linked list.
- * Approach 1 : Naive approach of iterating and remove all further duplicates of current node.
- * 							Space complexity O(1) & time complexity O(n^2)
- * Approach 2: Use a hash table, space complexity O(n), time complexity O(n)
- */
+* Cracking the coding interview edition 6
+* Problem 2-1 : Remove duplicates from an unsorted linked list.
+* Approach 1 : Naive approach of iterating and remove all further duplicates of current node.
+* 							Space complexity O(1) & time complexity O(n^2)
+* Approach 2: Use a hash table, space complexity O(n), time complexity O(n)
+*/
 
 
 #include <iostream>
@@ -18,11 +19,11 @@ struct Node {
 };
 
 /**
- * [insert - insert a node at the head of list]
- * @param head [head of the list]
- * @param data [new node's data]
- */
-void insert( Node * & head, int data )
+* [insert - insert a node at the head of list]
+* @param head [head of the list]
+* @param data [new node's data]
+*/
+void insert(Node * & head, int data)
 {
 	Node * newNode = new Node;
 	newNode->data = data;
@@ -31,11 +32,11 @@ void insert( Node * & head, int data )
 }
 
 /**
- * [printList Helper routine to print list]
- * @param head [head of the list]
- */
-void printList( Node * head ) {
-	while( head ) {
+* [printList Helper routine to print list]
+* @param head [head of the list]
+*/
+void printList(Node * head) {
+	while (head) {
 		std::cout << head->data << "-->";
 		head = head->next;
 	}
@@ -44,11 +45,11 @@ void printList( Node * head ) {
 
 //generate a random int between min and max
 /**
- * [random_range helper routine to generate a random number between min and max (including)]
- * @param  min [min of range]
- * @param  max [max of range]
- * @return     [A random number between min and max]
- */
+* [random_range helper routine to generate a random number between min and max (including)]
+* @param  min [min of range]
+* @param  max [max of range]
+* @return     [A random number between min and max]
+*/
 static inline int random_range(const int min, const int max) {
 	std::random_device rd;
 	std::mt19937 mt(rd());
@@ -61,20 +62,23 @@ static inline int random_range(const int min, const int max) {
 //space complexity O(1)
 // time complexity O(n^2)
 /**
- * [removeDuplicates Remove duplicates without using extra space]
- * @param head [head of list]
- */
-void removeDuplicates( Node * head ) {
-	if ( head == nullptr || ( head && (head->next == nullptr))) {
+* [removeDuplicates Remove duplicates without using extra space]
+* @param head [head of list]
+*/
+void removeDuplicates(Node * head) {
+	if (head == nullptr || (head && (head->next == nullptr))) {
 		return;
 	}
 	Node * curr = head;
-	while(curr) {
+	while (curr) {
 		Node * runner = curr;
 		while (runner->next != nullptr) {
 			if (runner->next->data == curr->data) {
+				Node *nodeToBeDeleted = runner->next;
 				runner->next = runner->next->next;
-			} else {
+				delete nodeToBeDeleted; // Free duplicate node
+			}
+			else {
 				runner = runner->next;
 			}
 		}
@@ -86,20 +90,22 @@ void removeDuplicates( Node * head ) {
 // space complexity - O(n)
 // time complexity - O(n)
 /**
- * [removeDuplicates1 - Remove duplicates from the list using hash table]
- * @param head [head of list]
- */
-void removeDuplicates1( Node * head ) {
-	if ( head == nullptr || ( head && (head->next == nullptr) )) {
-		return ;
+* [removeDuplicates1 - Remove duplicates from the list using hash table]
+* @param head [head of list]
+*/
+void removeDuplicates1(Node * head) {
+	if (head == nullptr || (head && (head->next == nullptr))) {
+		return;
 	}
 	std::unordered_map<int, int> node_map;
 	Node * prev = head;
 	Node * curr = head->next;
 	node_map[head->data] = 1;
-	while( curr != nullptr ) {
+	while (curr != nullptr) {
 		while (curr && node_map.find(curr->data) != node_map.end()) {
+			Node *nodeToBeDeleted = curr;
 			curr = curr->next;
+			delete nodeToBeDeleted; // Free duplicate node
 		}
 		prev->next = curr;
 		prev = curr;
@@ -110,13 +116,12 @@ void removeDuplicates1( Node * head ) {
 	}
 }
 
-
-
-int main() {
+int main()
+{
 	std::cout << "Method 1 : \n";
 	Node * head = nullptr;
-	for ( int i = 0; i < 10; ++i ) {
-		insert(head, random_range(1,7));
+	for (int i = 0; i < 10; ++i) {
+		insert(head, random_range(1, 4));
 	}
 	printList(head);
 	removeDuplicates(head);
@@ -124,8 +129,8 @@ int main() {
 
 	std::cout << "Method 2 : \n";
 	Node * head1 = nullptr;
-	for ( int i = 0; i < 10; ++i ) {
-		insert(head1, random_range(1,7));
+	for (int i = 0; i < 10; ++i) {
+		insert(head1, random_range(1, 4));
 	}
 	printList(head1);
 	removeDuplicates1(head1);
